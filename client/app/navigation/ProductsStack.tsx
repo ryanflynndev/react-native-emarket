3ee105d717f173b5614f5509661aefbc3eb03569
Ryan Flynn <ryanflynn44@gmail.com>
2024-03-29 18:21:26 -0400

    feat: added cart button

--- client/app/navigation/ProductsStack.tsx
+++ client/app/navigation/ProductsStack.tsx
@@ -1,7 +1,13 @@
-import { NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Products from "../screens/Products";
 import { ProductsStackParamList } from "./types";
 import ProductDetails from "../screens/ProductDetails";
+import CartModal from "../screens/CartModal";
+import { useNavigation } from "@react-navigation/native";
+import useCartStore from "../state/cartStore";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Ionicons } from '@expo/vector-icons';
 
 
 const ProductsStack = createNativeStackNavigator<ProductsStackParamList>();
@@ -17,12 +23,59 @@ const ProductsStackNav = () => {
         fontWeight: 'bold',
       },
       headerTintColor: '#333A73',
+      headerRight: cartButton,
     }}
   >
-    <ProductsStack.Screen name="Products" component={Products} options={{headerTitle: 'Ecomm'}} />
-    <ProductsStack.Screen name="ProductDetails" component={ProductDetails} options={{headerTitle: ''}} />
+    <ProductsStack.Screen name="Products" component={Products} options={{ headerTitle: 'Ecomm' }} />
+    <ProductsStack.Screen name="ProductDetails" component={ProductDetails} options={{ headerTitle: '' }} />
+    <ProductsStack.Screen name="CartModal" component={CartModal} options={{ headerShown: false, presentation: 'modal' }} />
   </ProductsStack.Navigator>
   );
-}
+};
+
+const cartButton = () => {
+  const navigation = useNavigation();
+  const { products } = useCartStore((state) => ({
+    products: state.products,
+  }));
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const productCount = products.reduce((prev, product) => prev += +product.quantity, 0);
+    setCount(productCount);
+  }, [products]);
+
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('CartModal')}>
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>
+          {count}
+        </Text>
+      </View>
+      <Ionicons name='cart' size={24} color={'#333A73'}></Ionicons>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  countContainer: {
+    position: 'absolute',
+    zIndex: 1,
+    bottom: -5,
+    right: -10,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  countText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#333A73'
+  },
+});
 
 export default ProductsStackNav;
--- client/app/navigation/types.ts
+++ client/app/navigation/types.ts
@@ -3,8 +3,11 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 export type ProductsStackParamList = {
   Products: undefined;
   ProductDetails: { id: number };
+  CartModal: undefined;
 };
 
 export type ProductsPageProps = NativeStackScreenProps<ProductsStackParamList, 'Products'>;
 
 export type ProductDetailsPageProps = NativeStackScreenProps<ProductsStackParamList, 'ProductDetails'>;
+
+export type CartModalPageProps = NativeStackScreenProps<ProductsStackParamList, 'CartModal'>;
